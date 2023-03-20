3f30ac467b21b412c0275cb1d7aedbbcd4148eb6
Sagar Chopra <schopra@firemate.com>
2023-03-20 16:47:43 +0530

    List rendering using for loop

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,23 @@
   <h2 v-else>This is Not a Number</h2>
 
   <h2 v-show="num === 0">This is not a zero</h2>
+
+  <h2 v-for="name in names" :key="name">{{ name }}</h2>
+  <h2 v-for="(name,index) in names" :key="name">{{ index }} {{ name }}</h2>
+
+  <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
+
+  <div v-for="actor in actors" :key="actor.name">
+    <h2>{{ actor.name }}</h2>
+    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
+  </div>
+
+  <h2 v-for="(value, key, index) in myInfo" :key="value">{{ index }} {{ key }} - {{ value }}</h2>
+
+  <template v-for="name in names" :key="name">
+    <h2>{{ name }}</h2>
+    <hr/>
+  </template>
 </template>
 
 <script>
@@ -80,7 +97,31 @@ export default {
         border: '1px solid red'
       },
 
-      num: 0
+      num: 0,
+
+      names: ['Bruce', 'Clark', 'Diana'],
+      fullNames:[
+        {first: 'Bruce', last:'Wayne'},
+        {first: 'Clark', last:'Kent'},
+        {first: 'Princess', last:'Diana'}
+      ],
+
+      actors:[
+       {
+          name: 'Christian Bale',
+          movies: ['Batman', 'The Prestige']
+       },
+       {
+          name: 'Di Caprio',
+          movies: ['Titanic', 'Inception']
+       }
+      ],
+
+      myInfo:{
+        name: 'Sagar',
+        channel: 'ChopCode',
+        course: 'Vue'
+      }
     }
   }
 }
