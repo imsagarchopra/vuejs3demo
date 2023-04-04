d870c2bd8645265d6415769e1da51e6b8325a99d
Sagar Chopra <schopra@firemate.com>
2023-04-04 17:02:21 +0530

    Composition api - ref vs reactive

--- src/components/DataComponent.vue
+++ src/components/DataComponent.vue
@@ -1,27 +1,53 @@
 <template>
    <h2>Options API - {{ o_firstName }}</h2>
+   <h2>Options API - {{ o_firstName }} {{ o_lastName }} a.k.a {{ o_heroName }}</h2>
+
    <h2>Composition API - {{ c_firstName }}</h2>
    <h2>Composition API - {{ greet }}</h2>
-</template>
+
+   <h2>Composition API - {{ c_firstName }} {{ c_lastName }} a.k.a {{ c_heroName }}</h2>
+   <h2>Composition API - {{ greetHero }}</h2>
+
+   <h2>Composition API - {{ state.firstName }} {{ state.lastName }} a.k.a {{ state.heroName }}</h2>
+   <h2>Composition API - {{ reactiveGreetHero }}</h2>
+</template> 
 
 <script>
-    import {ref} from 'vue';
+    import {ref, reactive} from 'vue';
     export default {
         name:'DataComponent',
         setup(){
             const c_firstName = ref('Clark')
+            const c_lastName = ref('Kent')
+            const c_heroName = ref('Superman')
             c_firstName.value = 'Diana'
 
             const greet = `Hello ${c_firstName.value}`
+            const greetHero = `Hello ${c_firstName.value} ${c_lastName.value} a.k.a ${c_heroName.value}`
+
+            const state = reactive({
+                firstName: 'Princess',
+                lastName: 'Diana',
+                heroName: 'Wonder Woman'
+            })
+ 
+            const reactiveGreetHero = `Hello ${state.firstName} ${state.lastName} a.k.a ${state.heroName}`
             console.log('c_firstName',c_firstName)
             return{
                 c_firstName,
-                greet
+                c_lastName,
+                c_heroName,
+                greet,
+                greetHero,
+                state,
+                reactiveGreetHero
             }
         },
         data(){
             return{
                 o_firstName: 'Bruce',
+                o_lastName: 'Wayne',
+                o_heroName: 'Batman'
                 
             }
         }
